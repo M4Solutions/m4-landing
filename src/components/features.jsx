d090fd7e332a325c8a5f0d3fc674fe8d4972e94d
Hassan Yahya <hassanyahya400@gmail.com>
2023-02-12 23:46:30 +0100

    add services card header

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -42,15 +42,15 @@ const Features = () => {
                             <div key={item.title} className="p-4 md:w-1/3">
                                 <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
                                 <div className="flex items-center mb-3">
-                                    <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-light-blue text-white flex-shrink-0">
+                                    <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-light-blue text-white flex-shrink-0">
                                     <FontAwesomeIcon icon={item.icon} />
                                     </div>
                                     <h2 className="text-gray-900 text-lg title-font font-medium">{item.title}</h2>
                                 </div>
-                                <div class="flex-grow">
+                                <div className="flex-grow">
                                     <p className="leading-relaxed text-base">{item.desc}</p>
                                     <a href='/' className="mt-3 text-light-blue inline-flex items-center">Learn More
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                                     </svg>
                                     </a>
